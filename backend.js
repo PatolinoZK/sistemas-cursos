99b41c7f7e2e9936d6e0d32a20d1710682250897
FakerllJ <campelojvc@gmail.com>
2026-04-23 20:39:33 -0300

    Adição do primeiro curso

---
+++ backend.js
@@ -0,0 +1,15 @@
+let cursos = [];
+function adicionarCurso(nome, carga, descricao) {
+  if (!nome || !carga || !descricao){
+  alert("Preencha todos os campos!");
+  return
+  }
+  
+  let curso = {
+    Design Gráfico,
+    360,
+    Curso que visa capacitar e aperfeiçoar os alunos na edição de imagens e vídeos,
+  };
+
+  cursos.push(curso);
+}
